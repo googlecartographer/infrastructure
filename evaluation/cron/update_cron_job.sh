8a18a8652d288a528114fec31f458f82c00a2d21
Sebastian Klose <klose@google.com>
2018-08-14 09:51:47 +0200

    Cron job WIP

---
+++ evaluation/cron/update_cron_job.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -o errexit
+set -o verbose
+
+docker build -t eu.gcr.io/robco-klose/nightly_cron -f nightly_cron.Dockerfile .
+docker push eu.gcr.io/robco-klose/nightly_cron
+
+#kubectl delete cronjobs nightly-evaluation
+#kubectl create -f k8s_nightly_cronjob.yaml
