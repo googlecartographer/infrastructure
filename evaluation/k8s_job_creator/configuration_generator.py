5604fd77c64fcff4a32cdf75b7fa5f9e3697565d
Sebastian Klose <klose@google.com>
2018-08-28 17:04:56 +0200

    Minor cleanups

--- evaluation/k8s_job_creator/configuration_generator.py
+++ evaluation/k8s_job_creator/configuration_generator.py
@@ -6,18 +6,15 @@ for parameter sweeps.
 
 from __future__ import absolute_import
 
-from absl import app
-from absl import flags
 from absl import logging
 from google.cloud import storage
-
-FLAGS = flags.FLAGS
-flags.DEFINE_string('sweep_file', None, 'parameter sweep file')
-flags.DEFINE_string('cfg_base', None, 'Configuration basefile')
 
 
 def upload_to_cloud_bucket(src_file, destination_bucket, destination,
                            secret_json):
+  """ Helper for uploading a file to cloud storage.
+
+  """
   client = storage.Client.from_service_account_json(secret_json)
   try:
     bucket = client.bucket(destination_bucket)
@@ -31,6 +28,9 @@ def upload_to_cloud_bucket(src_file, destination_bucket, destination,
 
 
 def load_sweep_file(filename):
+  """ Loads file and returns single lines split by empty space.
+
+  """
   with open(filename, 'rb') as f:
     for line in f:
       items = line.split(' ')
@@ -40,6 +40,7 @@ def load_sweep_file(filename):
 
 def load_base_config_file(filename):
   """ Loads file into list of lines, removing the "return options" line.
+
   """
   with open(filename, 'r') as cfg_file:
     return [l for l in cfg_file.readlines() if not 'return options' in l]
@@ -60,17 +61,19 @@ def write_config_file(base, swept_cfgs, output_file):
 
 
 class ParameterSweep:
-  """ Represents a sweep for a single parameter.
+  """ A sweep for a single parameter.
+
+  Represents a single parameter and the values it should take in the sweep.
   """
 
   def __init__(self, parameter_with_values):
     self._param_name = parameter_with_values[0].strip()
     self._values = [v.strip() for v in parameter_with_values[1:]]
 
-  def parameter_dimension(self):
+  def dimension(self):
     return len(self._values)
 
-  def parameter_name(self):
+  def name(self):
     return self._param_name
 
   def value_for_index(self, index):
@@ -80,7 +83,7 @@ class ParameterSweep:
 def sweep_parameters(sweep_file_name):
   """ Sweeps through all parameter combinations.
 
-  Yields a dictionary with <parameter_name : current_value> items in each
+  Yields a dictionary with <name : current_value> items in each
   iteration.
   """
   parameter_sweep = [
@@ -88,38 +91,20 @@ def sweep_parameters(sweep_file_name):
   ]
   current_index = [0 for _ in parameter_sweep]
 
-  while current_index[0] < parameter_sweep[0].parameter_dimension():
+  while current_index[0] < parameter_sweep[0].dimension():
     config_instance = dict()
     for i, p in enumerate(parameter_sweep):
-      config_instance[p.parameter_name()] = p.value_for_index(current_index[i])
+      config_instance[p.name()] = p.value_for_index(current_index[i])
     yield config_instance
 
     increment_next = True
     idx = len(parameter_sweep) - 1
     while increment_next and idx >= 0:
       current_index[idx] += 1
-      if current_index[idx] >= parameter_sweep[idx].parameter_dimension():
+      if current_index[idx] >= parameter_sweep[idx].dimension():
         if idx != 0:
           current_index[idx] = 0
         increment_next = True
       else:
         increment_next = False
       idx -= 1
-
-
-def main(argv):
-  if len(argv) > 1:
-    raise app.UsageError('Too many command-line arguments.')
-
-  if FLAGS.sweep_file:
-    for parameter_dict in generate_configurations(FLAGS.sweep_file):
-      logging.info('Current sweep')
-      for k, v in parameter_dict.iteritems():
-        logging.info('%s   -->   %s', k, v)
-  if FLAGS.cfg_base:
-    for r in load_base_config_file(FLAGS.cfg_base):
-      logging.info(r)
-
-
-if __name__ == '__main__':
-  app.run(main)
