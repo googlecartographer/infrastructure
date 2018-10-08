7d1f0dfc77b87d09681f3b94bf5fdb97fa5177d0
Sebastian Klose <klose@google.com>
2018-10-08 17:20:04 +0200

    updating nightly
    * more datasets
    * kicking off at 18:00 UTC

--- evaluation/cron/update_cron_job.sh
+++ evaluation/cron/update_cron_job.sh
@@ -5,5 +5,6 @@ set -o verbose
 docker build -t eu.gcr.io/robco-klose/nightly_cron -f nightly_cron.Dockerfile ..
 docker push eu.gcr.io/robco-klose/nightly_cron
 
-#kubectl delete cronjobs nightly-evaluation
-#kubectl create -f k8s_nightly_cronjob.yaml
+# currently we schedule jobs "nightly" at 20:00 UTC.
+kubectl delete cronjobs nightly-evaluation
+kubectl create -f k8s_nightly_cronjob.yaml
